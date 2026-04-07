8505b07b8a2e43fda67160ea036ced177877a904
remixed123 <glennvassallo@hotmail.com>
2026-04-07 22:05:04 +1000

    Initial Commit

---
+++ WordSequence/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WordSequence
+//
+//  Created by Glenn Vassallo on 7/4/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WordSequence/WordSequenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordSequenceApp.swift
+//  WordSequence
+//
+//  Created by Glenn Vassallo on 7/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct WordSequenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
